f1be876fa524b4eb2e3bb486dcc339c9a26969b5
saifobe <obeidatsaif93@gmail.com>
2023-03-24 12:45:44 +0000

    route that take data from API done

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 // server.js
 const express = require('express')
-// const userRoute = require('./users/controller')
 const apiRoute = require('./controllers/api/apiGet')
 
 const server = express();
 
 // server.use('/users',userRoute)
+
+//http://localhost:3000
 server.use('/',apiRoute)
 
 server.listen(3000,() => console.log("start server")) 
