5b68579b90e8992f636877d063d680a1a4046cd4
Emam Mounqer <emam.anagreh@gmail.com>
2023-03-31 20:52:53 +0000

    add route to get the data from alpha of country

--- controllers/api/apiGet.js
+++ controllers/api/apiGet.js
@@ -5,7 +5,7 @@ const express = require("express")
 const apiRoute = express.Router();
 
 const axios = require('axios');
-
+const lookup = require('country-code-lookup')
 
 require('dotenv').config();
 
@@ -14,8 +14,43 @@ apiRoute.get('/', (req, res) => {
         const ApiURL = process.env.ApiURL;
         axios.get(ApiURL)
             .then((result) => {
-                console.log(result);
-                res.send(result.data);
+                const allCountry = result.data.filter((country) => country.cca3 !== 'ISR')
+                res.send(allCountry);
+
+            })
+
+            .catch((err) => {
+                console.log("sorry,something went wrong", err);
+                res.status(500).send(err);
+            })
+    }
+
+    catch {
+        
+    }
+})
+
+
+apiRoute.get('/:title', (req, res) => {
+    try {
+        const title = req.params.title;
+        const ApiURL = `https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&titles=${title}&exintro&explaintext&exsentences=3`;
+        const oneCountryAPI = `https://restcountries.com/v3.1/name/${title}`
+        axios.get(ApiURL)
+            .then((result) => {
+                axios.get(oneCountryAPI)
+                    .then(item => {
+                        const overview = result.data.query.pages;
+                        const objValue = Object.values(overview);
+                        const myItem = item.data[0];
+                        const allData = {...myItem, overview: objValue[0]?.extract}
+                        console.log(item);
+                        res.send(allData);
+                    })
+                    .catch((err) => {
+                        console.log("sorry,something went wrong", err);
+                        res.status(500).send(err);
+                    })
 
             })
 
@@ -30,12 +65,17 @@ apiRoute.get('/', (req, res) => {
     }
 })
 
-
-apiRoute.get('/:title', (req, res) => {
+apiRoute.get('/alpha/:id', (req, res) => {
     try {
-        const title = req.params.title;
-        const ApiURL = `https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&titles=${title}&exintro&explaintext&exsentences=3`;
-        const oneCountryAPI = `https://restcountries.com/v3.1/name/${title}`
+        const id = req.params.id;
+        let country = id;
+        try {
+            country = lookup.byIso(id)
+        } catch (error) {
+            return res.status(400).send({message: 'no country with that id was found'})
+        }
+        const ApiURL = `https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&titles=${country.country}&exintro&explaintext&exsentences=3`;
+        const oneCountryAPI = `https://restcountries.com/v3.1/alpha/${id}`
         axios.get(ApiURL)
             .then((result) => {
                 axios.get(oneCountryAPI)
